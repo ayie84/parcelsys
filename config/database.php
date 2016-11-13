e0c4e602155388b824c00b793db71417e7af1299
gezzeg <mr.ghazali@gmail.com>
2016-11-13 23:57:07 +0800

    edit database.php

--- config/database.php
+++ config/database.php
@@ -1,5 +1,8 @@
+	<?php
+	
 	$user_name = "walimatu_postmel"; //username
 	$password = "Pusatmel2016";//password
 	$database = "walimatu_pusatmel";//dbname
 	$host_name = "localhost"; //server
-	
+
+?>	
