ffce14721d53307b0ff1cf0822c91417bb9e4b1e
ayie84 <ahmadfitri@ump.edu.my>
2016-11-13 21:08:14 +0800

    Update tracking_result.php

--- tracking_result.php
+++ tracking_result.php
@@ -7,7 +7,7 @@ con2db();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>UMP Tracking Result..</title>
+  <title>Parcel Tracking Result..</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="inc/css/bootstrap.min.css">
