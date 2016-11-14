0c5baaef3cf437210c035921c35413322e922390
Ahmad Fitri <ahmadfitri@ump.edu.my>
2016-11-14 14:51:34 +0800

    login pages index file

--- index.php
+++ index.php
@@ -12,8 +12,56 @@ debugScript(); //comment this line for debug error msg
 
 con2db();
 pageTitle("Parcel Management System");
-include 'inc/header.php';
-echo 'index file.php';
-parcelReportToday();
-include 'inc/footer.php';
+include 'inc/headerIndex.php';
+?>
+ <div class="container" style="margin-top:40px">
+		<div class="row spacer">
+			<div class="col-sm-6 col-md-4 col-md-offset-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<strong> Parcel Management System</strong>
+					</div>
+					<div class="panel-body">
+						<form role="form" action="#" method="POST">
+							<fieldset>
+								<div class="row">
+									<div class="center-block spacer" align="center">
+										
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-sm-12 col-md-10  col-md-offset-1 ">
+										<div class="form-group">
+											<div class="input-group">
+												<span class="input-group-addon">
+													<i class="glyphicon glyphicon-user"></i>
+												</span> 
+												<input class="form-control" placeholder="Username" name="loginname" type="text" autofocus>
+											</div>
+										</div>
+										<div class="form-group">
+											<div class="input-group">
+												<span class="input-group-addon">
+													<i class="glyphicon glyphicon-lock"></i>
+												</span>
+												<input class="form-control" placeholder="Password" name="password" type="password" value="">
+											</div>
+										</div>
+										<div class="form-group">
+											<input type="submit" class="btn btn-lg btn-primary btn-block" value="Sign in">
+										</div>
+									</div>
+								</div>
+							</fieldset>
+						</form>
+					</div>
+					<div class="panel-footer ">
+						<h6 align="center">Hakcipta © 2013 Universiti Malaysia Pahang. </br>Hak cipta terpelihara.</h6>
+					</div>
+                </div>
+			</div>
+		</div>
+	</div>
+<?php
+//include 'inc/footer.php';
 ?>
