5074c81979dfef8b84bcbb37b6e06bbdfaffa7fd
Ahmad Fitri <ahmadfitri@ump.edu.my>
2016-11-14 09:20:18 +0800

    modify result.php

--- result.php
+++ result.php
@@ -1,42 +1,17 @@
 <?php
 /*
-10-11-2016
-parcel.php
--store & view courier company
+@Title 		: Parcel Management System
+@Filename 	: result.php
+@Author		: Fit3
+@date		: 13-11-16
 
 */
 include 'inc/function.php';
+
 con2db();
-//$result = track();
-//echo 'parcel.php file';
+pageTitle("Parcel Query Result");
+include 'inc/header.php';
+
+track();
+include 'inc/footer.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>UMP Parcel</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="inc/css/bootstrap.min.css">
-  <link rel="stylesheet" href="inc/css/style.min.css">
-  <script src="inc/js/jquery.min.js"></script>
-  <script src="inc/js/bootstrap.min.js"></script>
-  <style>
-	.spacer { margin-top: 40px;}
-  </style>
-</head>
-<body>
-
-<?php navbar(); ?>
-  
-<div class="container  spacer">
-
-	
-	<?php
-		track();
-		?>
-
-</div>
-
-</body>
-</html>
-
